d2bc1b3813de7e475f173642fe294b1db8c629bf
vusbeyt <98389388+vusbeyt@users.noreply.github.com>
2022-09-28 16:24:23 +0300

    fix сделанного

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-import Logo from './views/logo.vue'
 import Navbar from './views/Navbar.vue'
 
 import './assets/main.css'
 import './assets/scss/logo.scss'
 import './assets/scss/blocks.scss'
+import './assets/scss/vectors.scss'
 
 const app = createApp(App)
 
@@ -15,5 +15,4 @@ app.use(router)
 
 app.mount('#app')
 
-app.component("Logo", Logo);
 app.component("Navbar", Navbar);
